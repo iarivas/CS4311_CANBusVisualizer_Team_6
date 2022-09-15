57dadc9bdc785ff71b5363b64bc17b815d24274a
Justus frausto <jfrausto10@gmail.com>
2022-09-14 20:04:03 -0600

    Added barebone projectManager class. currently no logic is added but can create default project object on def call.

---
+++ projectManager.py
@@ -0,0 +1,29 @@
+'''
+Last Updated 9/14/2022 by justus
+ 
+Will update project settings and state as more
+information becomes available to us
+
+Current state of project manager is barebones 
+skeleton containing definitions but no logic.
+Currently creates new project on createProject 
+def call
+'''
+import projectConfig
+
+class projectManager():
+
+    currentProject = None #Will create project object to store information off file
+    projectState = None #temp file call
+    
+    def __init__(self) -> None:
+        pass
+
+    def createProject():
+        currentProject = projectConfig.project()
+
+    def openProject():
+        return
+
+    def archiveProject():
+        return
