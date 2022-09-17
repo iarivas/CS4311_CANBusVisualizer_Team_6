9877c919bcbe55df6190d689a91c7a84fac33178
Montserrat Molina <mgmolina3@miners.utep.edu>
2022-09-17 15:34:11 -0600

    CBV-30 creating Node and nodeManager

---
+++ Node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, ID, name, position, annotation, relatedNodeID, color, icon, visitibility):
+        self._ID = ID
+        self._name = name
+        self._position = position
+        self._annotation = annotation
+        self._relatedNodeID = relatedNodeID
+        self._color = color
+        self._icon = icon
+        self._visitibility = visitibility
