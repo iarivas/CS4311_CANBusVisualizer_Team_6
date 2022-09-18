4d089231666f0b7c44595ec792ce83b69ee15991
ianrigsbee54 <irrigsbee@miners.utep.edu>
2022-09-17 20:11:36 -0600

    blacklist manager and temp file manager.

---
+++ blacklistManager.py
@@ -0,0 +1,22 @@
+
+#when Node and filemanager are implemented Ill edit this class -Ian
+class blacklistManager:
+    def __init__(self, listOfBlacklistedNodes):
+        self._listOfBlacklistedNodes = listOfBlacklistedNodes
+
+    def setListOfBlacklistedNodes():
+        return
+        #TODO: finish this thang
+    
+    def checkIfNodeIsBlacklisted():
+        return
+        #TODO: finish the fight
+
+    def importNodes():
+        #listOfBlacklistedNodes = projectManager.importNodes()
+        return
+        #TODO: finish it
+
+    def exportNodes():
+        return
+        #TODO: you already know what imma put
