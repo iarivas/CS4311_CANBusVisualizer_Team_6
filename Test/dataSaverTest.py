c3a46950f0a1cfcf68e918559d51a265c1dd6e7e
LazyLinoone <Dillontrinh120@gmail.com>
2022-09-21 10:07:21 -0600

    updated file reference

--- Test/dataSaverTest.py
+++ Test/dataSaverTest.py
@@ -1,11 +1,11 @@
 import sys
  
 # setting path
-sys.path.append('..')
+sys.path.append('../src')
 
 from dataSaver import *
 
 # change 1st variable if testing create project
 # dataSaver.createInitialProject(2, 2, 3, 4, None, None)
 
-dataSaver.update(1, 3, 5, 7, 9, 11)
+dataSaver.update(2, 3, 5, 8, 9, 11)
