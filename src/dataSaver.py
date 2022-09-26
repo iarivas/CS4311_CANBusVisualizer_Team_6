4d800d249eea5e47470cbaa07d1afcf5b28e021e
Montserrat Molina <mgmolina3@miners.utep.edu>
2022-09-26 08:47:21 -0600

    CBV-61 Adding local database integration

--- src/dataSaver.py
+++ src/dataSaver.py
@@ -33,9 +33,14 @@ class dataSaver:
         x = _mycol.update_one(olddoc, newdoc)
 
     def createInitialProject(projectID, baudRate, initials, name, dbcFile, blacklistFile):
-        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
+        # TODO: need to add a way to check if analyst has wifi or not in order to decide which databse to save to
+        # Also not sure how to sync local database with the online database for when the analyst gets wifi access back...
+        hasWifi = True
+        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test") if hasWifi else pymongo.MongoClient("mongodb://localhost:27017")
         _mydb = _myclient["TestDB"]
         _mycol = _mydb["TestCol"]
+
+        baudRate = 12
 
         if baudRate != None and initials != None:
 
