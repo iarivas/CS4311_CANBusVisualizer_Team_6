462e4e494156499a712f9d39d30cf28d633cf3b1
LazyLinoone <Dillontrinh120@gmail.com>
2022-09-28 12:32:31 -0600

    created methods to get data about projects from database

--- Test/dataReceiverTest.py
+++ Test/dataReceiverTest.py
@@ -5,4 +5,14 @@ sys.path.append('../src')
 
 from dataReceiver import *
 
-dataReceiver.retrieveProject(2)
+# dataReceiver.retrieveProject(2)
+
+# dataReceiver.retrieveBaudRate("Project 1")
+# dataReceiver.retrieveBlacklistFile("Project 2")
+# dataReceiver.retrieveName("TestingLocal")
+# dataReceiver.retrieveDBCFile("TestingLocal")
+# dataReceiver.retrieveInitials("Project 2")
+# dataReceiver.retrieveProjects("Project 2")
+
+projects = dataReceiver.retrieveAllProjects()
+print(projects)
--- src/dataReceiver.py
+++ src/dataReceiver.py
@@ -14,6 +14,19 @@ class dataReceiver:
     def createTempFile(self, something):
         ...
 
+    def retrieveAllProjects():
+        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
+        _mydb = _myclient["TestDB"]
+        _mycol = _mydb["TestCol"]
+
+        store = []
+
+        for x in _mycol.find({} , {"_id": 1, "eventName": 1}):
+            print(x)
+            store.append(x)
+        
+        return store
+
     def retrieveProject(projectID):
         _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
         _mydb = _myclient["TestDB"]
@@ -24,3 +37,51 @@ class dataReceiver:
         })
 
         print(x)
+
+    def retrieveName(projectID):
+        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
+        _mydb = _myclient["TestDB"]
+        _mycol = _mydb["TestCol"]
+
+        for x in _mycol.find({"_id": projectID}, {"_id": 0, "eventName": 1}):
+            print(x)
+
+    def retrieveBaudRate(projectID):
+        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
+        _mydb = _myclient["TestDB"]
+        _mycol = _mydb["TestCol"]
+
+        for x in _mycol.find({"_id": projectID}, {"_id": 0, "baudRate": 1}):
+            print(x)
+    
+    def retrieveDBCFile(projectID):
+        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
+        _mydb = _myclient["TestDB"]
+        _mycol = _mydb["TestCol"]
+
+        for x in _mycol.find({"_id": projectID}, {"_id": 0, "dbcFile": 1}):
+            print(x)
+
+    def retrieveBlacklistFile(projectID):
+        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
+        _mydb = _myclient["TestDB"]
+        _mycol = _mydb["TestCol"]
+
+        for x in _mycol.find({"_id": projectID}, {"_id": 0, "blacklistFile": 1}):
+            print(x)
+    
+    def retrieveInitials(projectID):
+        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
+        _mydb = _myclient["TestDB"]
+        _mycol = _mydb["TestCol"]
+
+        for x in _mycol.find({"_id": projectID}, {"_id": 0, "initials": 1}):
+            print(x)
+    
+    def retrieveProjects(eventName):
+        _myclient = pymongo.MongoClient("mongodb+srv://Dillon:v4nbq3GP8Cyb3p4@software2.akghm64.mongodb.net/test")
+        _mydb = _myclient["TestDB"]
+        _mycol = _mydb["TestCol"]
+
+        for x in _mycol.find({"eventName": eventName}):
+            print(x)
