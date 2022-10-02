5b1426e41fd7251945ad911315269eecd8f8831c
Montserrat Molina <mgmolina3@miners.utep.edu>
2022-10-02 13:10:55 -0600

    CBV-84 Added get packets API and refactored code

--- src/projectManager.py
+++ src/projectManager.py
@@ -1,5 +1,5 @@
 '''
-Last Updated 9/19/2022 by montse
+Last Updated 10/92/2022 by Montse
  
 Will update project settings and state as more
 information becomes available to us
@@ -9,36 +9,11 @@ skeleton containing definitions but no logic.
 Currently creates new project on createProject 
 API endpoint call
 '''
-from curses import baudrate
-from urllib import response
 import projectConfig
-from fastapi import FastAPI, File, UploadFile, status, HTTPException
-from pydantic import BaseModel
-from dataSaver import *
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-class ProjectInfo(BaseModel):
-    baudRate: int
-    initials: str
-    eventName: str
-    dbcFile: str = None
-    blacklistFile: str = None
 
 class projectManager():
 
-    currentProject = None #Will create project object to store information off file
+    currentProject: projectConfig.project = None #Will create project object to store information off file
     projectState = None #temp file call
     
     def __init__(self) -> None:
@@ -49,25 +24,3 @@ class projectManager():
 
     def archiveProject():
         return
-
-    @app.post("/projects/")
-    def createProject(projectInfo: ProjectInfo):
-        if len(projectInfo.initials) == 0:
-            raise HTTPException(
-                status_code = status.HTTP_400_BAD_REQUEST,
-                detail = "Analyst initials were not provided"
-            )
-        if baudrate is None:
-            raise HTTPException(
-                status_code = status.HTTP_400_BAD_REQUEST,
-                detail = "Baud rate was not provided"
-            )
-        if len(projectInfo.eventName) == 0:
-            raise HTTPException(
-                status_code = status.HTTP_400_BAD_REQUEST,
-                detail = "Project name was not provided"
-            )
-        currentProject = projectConfig.project(projectInfo.baudRate, projectInfo.initials, projectInfo.eventName, projectInfo.dbcFile, projectInfo.blacklistFile)
-        # createInitialPoject is the mongoDB saving definition from dataSaver.py 
-        dataSaver.createInitialProject(currentProject.projectId, currentProject.baudRate, currentProject.analystInitials, currentProject.eventName, currentProject.dbcFileName, currentProject.blackListFileName)
-        return currentProject
