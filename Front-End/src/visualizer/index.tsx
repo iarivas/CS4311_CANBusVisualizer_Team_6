c99cf37f518645c8336e2cbf89980cd726e58195
Alan Perez <alanperezse@gmail.com>
2022-10-12 11:16:37 -0600

    Added initial code

--- Front-End/src/visualizer/index.tsx
+++ Front-End/src/visualizer/index.tsx
@@ -93,6 +93,67 @@ function Visualizer() {
         api.gatherTraffic(play, projectId)
     }
 
+    // Map and nodes
+    let [mapState, setMapState] = useState({
+        nodeDataArray: [
+            { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0' },
+            { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
+            { key: 2, text: 'Gamma', color: 'lightgreen', loc: '0 150' },
+            { key: 3, text: 'Delta', color: 'pink', loc: '150 150' }
+        ],
+        linkDataArray: [
+            { key: -1, from: 0, to: 1 },
+            { key: -2, from: 0, to: 2 },
+            { key: -3, from: 1, to: 1 },
+            { key: -4, from: 2, to: 3 },
+            { key: -5, from: 3, to: 0 }
+        ],
+        modelData: {
+            canRelink: true
+        },
+        selectedKey: null,
+        skipsDiagramUpdate: false
+    })
+
+    const handleDiagramEvent = (e: go.DiagramEvent) => {
+        const name = e.name;
+        switch (name) {
+          case 'ChangedSelection': {
+            const sel = e.subject.first();
+            if (sel) {
+              setMapState({...mapState, selectedKey: sel.key });
+            } else {
+              setMapState({...mapState, selectedKey: null });
+            }
+            break;
+          }
+          default: break;
+        }
+    }
+
+    /**
+     * Handle GoJS model changes, which output an object of data changes via Model.toIncrementalData.
+     * This method should iterates over those changes and update state to keep in sync with the GoJS model.
+     * This can be done via setState in React or another preferred state management method.
+     * @param obj a JSON-formatted string
+     */
+    const handleModelChange = (obj: go.IncrementalData) => {
+        // const insertedNodeKeys = obj.insertedNodeKeys;
+        // const modifiedNodeData = obj.modifiedNodeData;
+        // const removedNodeKeys = obj.removedNodeKeys;
+        // const insertedLinkKeys = obj.insertedLinkKeys;
+        // const modifiedLinkData = obj.modifiedLinkData;
+        // const removedLinkKeys = obj.removedLinkKeys;
+        // const modifiedModelData = obj.modelData;
+
+        console.log(obj);
+    }
+
+    // const handleRelinkChange = (e: any) => {
+    //     const target = e.target;
+    //     const value = target.checked;
+    //     setMapState({...mapState, modelData: { canRelink: value }, skipsDiagramUpdate: false });
+    // }
     
     // Other stuff
     
@@ -120,7 +181,14 @@ function Visualizer() {
                     />
                 </div>
                 <div className='node-map-container-content'>
-                    <NodeMap></NodeMap>
+                    <NodeMap
+                        nodeDataArray={mapState.nodeDataArray}
+                        linkDataArray={mapState.linkDataArray}
+                        modelData={mapState.modelData}
+                        skipsDiagramUpdate={mapState.skipsDiagramUpdate}
+                        onDiagramEvent={handleDiagramEvent}
+                        onModelChange={handleModelChange}
+                    />
                 </div>
             </div>
         </div>
--- Front-End/src/visualizer/nodeMap/index.tsx
+++ Front-End/src/visualizer/nodeMap/index.tsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
+import DiagramWrapper from './DiagramWrapper'
 import './index.css'
 
-function NodeMap() {
+
+function NodeMap({
+        nodeDataArray,
+        linkDataArray,
+        modelData,
+        skipsDiagramUpdate,
+        onDiagramEvent,
+        onModelChange,
+    }: any) {
     return (
         <div className='node-map-container rounded'>
             <div className='node-map-container-inner'>
                 <h3>CAN Bus Map</h3>
+            </div>
+            <div className='node-map rounded'>
+                    <DiagramWrapper
+                    nodeDataArray={nodeDataArray}
+                    linkDataArray={linkDataArray}
+                    modelData={modelData}
+                    skipsDiagramUpdate={skipsDiagramUpdate}
+                    onDiagramEvent={onDiagramEvent}
+                    onModelChange={onModelChange}
+                />
             </div>
         </div>
     );
