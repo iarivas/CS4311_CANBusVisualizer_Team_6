80c06d47394375ce03f1842128ee642eb7c430b2
ianrigsbee54 <irrigsbee@miners.utep.edu>
2022-10-19 11:43:43 -0600

    it work bbs

--- Front-End/src/visualizer/nodeMap/index.tsx
+++ Front-End/src/visualizer/nodeMap/index.tsx
@@ -9,6 +9,7 @@ function NodeMap({
     nodes,
     onNodesChange,
     onEdgesChange,
+    onNodeContextMenu
     }: any) {
       
     return (
@@ -23,6 +24,7 @@ function NodeMap({
                     nodes={nodes}
                     onNodesChange={onNodesChange}
                     onEdgesChange={onEdgesChange}
+                    onNodeContextMenu={onNodeContextMenu}
                 >
                     <Controls/>
                 </ReactFlow>
