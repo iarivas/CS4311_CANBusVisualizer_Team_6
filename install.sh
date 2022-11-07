440253674aa01a6fdde6e2ad0af559de5ce77a97
irrigsbee <irrigsbee@miners.utep.edu>
2022-11-07 16:39:43 -0500

    initial mongodb installation, will test on dummy

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ declare runOption="run";
 
 function installer {
     pip --version;
+    wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -;
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list;
+    sudo apt-get update -y;
+    sudo apt-install -y mongodb-org;
     echo "installing";
 }
 function runner {
