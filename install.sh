8182bc850321288598a04abfe0eac5f428c93339
irrigsbee <irrigsbee@miners.utep.edu>
2022-11-07 17:26:30 -0500

    added python module installation, it all works

--- install.sh
+++ install.sh
@@ -5,12 +5,30 @@ declare installOption="install";
 declare runOption="run";
 
 function installer {
-    pip --version;
+    #install mongodb community edition.
+    installMongoDbCommunity;
+    #install mongodb compass
+    installMongoDbCompass;
+    #install python modules
+    installPythonModules;
+    echo "installing";
+}
+function installPythonModules {
+    python3 -m pip install fastapi;
+    python3 -m pip install uvicorn[standard];
+    python3 -m pip install pymongo;
+    python3 -m pip install python-can;
+    python3 -m pip install cantools;
+}
+function installMongoDbCompass {
+    wget https://downloads.mongodb.com/compass/mongodb-compass_1.33.1_amd64.deb;
+    sudo dpkg -i mongodb-compass_1.33.1_amd64.deb;
+}
+function installMongoDbCommunity {
     wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -;
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list;
     sudo apt-get update -y;
     sudo apt-install -y mongodb-org;
-    echo "installing";
 }
 function runner {
     echo "running"
